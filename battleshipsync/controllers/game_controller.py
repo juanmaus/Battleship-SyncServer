5db8e1c88653fb24343021af217887539d186026
Juanma <juanma@juanma.us>
2017-08-06 04:50:29 -0600

    Seguir en laptop, por algún motivo no me funciona autenticación jwt desde desktop.

--- battleshipsync/controllers/game_controller.py
+++ battleshipsync/controllers/game_controller.py
@@ -1,4 +1,4 @@
-from battleshipsync import app
+from battleshipsync import app, redis_store
 from flask import request, jsonify
 from battleshipsync.models.account import User
 from battleshipsync.models.account import UserService
@@ -36,3 +36,32 @@ def post_game():
 @enable_jsonp
 def get_game(game_id):
     pass
+
+
+@app.route('/api/v1/game/setBoard', methods=['POST'])
+@jwt_required()
+@enable_jsonp
+def index():
+    board_data = request.get_json()
+    key = board_data['key']             #divide gameId and playerId
+    board = board_data['board']
+    redis_store.set(key, board)
+    return "someting more usefull"
+
+##--example payaload"
+
+# {
+#             "key": "5:12",
+#             "board":[
+#                  [0,0,0,5,5,5,5,5,0,0],
+#                  [2,2,0,0,0,0,0,0,0,0],
+#                  [0,0,1,0,0,0,4,0,0,0],
+#                  [0,0,0,0,0,0,4,0,0,0],
+#                  [0,0,0,0,0,0,4,0,0,0],
+#                  [0,0,0,0,0,0,4,0,0,0],
+#                  [0,0,2,0,1,0,0,0,0,0],
+#                  [0,0,2,0,0,0,0,0,3,0],
+#                  [0,0,0,0,0,0,0,0,3,0],
+#                  [3,3,3,0,0,0,0,0,3,0]
+#              ]
+# } go on from here, resolve jwt auth problems
