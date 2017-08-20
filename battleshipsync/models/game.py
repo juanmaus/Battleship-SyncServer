1d50acb36c24bf36038e233d380a568678136f2b
Felix <juanfe190@hotmail.com>
2017-08-20 00:12:12 -0600

    Start a game with owner as first player, add method to move to next player

--- battleshipsync/models/game.py
+++ battleshipsync/models/game.py
@@ -29,8 +29,8 @@ class GameStatus(Enum):
 class GameMode(Enum):
 
     """
-        Defines the mode in which a game should be created. The game mode determines the 
-        amount of players required to get the game started and the amount of boards that 
+        Defines the mode in which a game should be created. The game mode determines the
+        amount of players required to get the game started and the amount of boards that
         need to be generated.
     """
     PLAYER_2v2 = 2
@@ -41,17 +41,17 @@ class GameMode(Enum):
 # CLASS GAME
 # ---------------------------------------------------------------------------------------
 class Game:
-    """ 
-        This class represents a game in the system. Every time a new game is created, a 
+    """
+        This class represents a game in the system. Every time a new game is created, a
         unique uuid4 identifier is assigned to the game so it can be uniquely referenced.
         Game instances are persisted using REDIS in-memory data structure storage so that
         the game state is persistent across multiple http requests originated by different
-        player. 
-        
-        A game can be created by any registered player in the system an can be created in 
+        player.
+
+        A game can be created by any registered player in the system an can be created in
         two different modes: 2-player mode and 4-player mode. Initially games are created
-        with WAITING_FOR_PLAYERS status which sets the game in waiting mode until the 
-        required amount of players have joined the game. 
+        with WAITING_FOR_PLAYERS status which sets the game in waiting mode until the
+        required amount of players have joined the game.
     """
 
     # -----------------------------------------------------------------------------------
@@ -69,7 +69,7 @@ class Game:
         """
             :param mode: GameMode type. Can be 4PLAYER mode or 2PLAYER mode
             :param owner: The id of the user that created the game.
-            :param player_layout: A list of the player types that can join the game. 
+            :param player_layout: A list of the player types that can join the game.
         """
         self.id = str(uuid.uuid4())
         self.timestamp = datetime.datetime.now()
@@ -78,7 +78,7 @@ class Game:
         self.owner = owner
         self.moves_next = owner
         self.player_layout = player_layout
-        self.players = []
+        self.players = [owner]
         self.__open_spots = player_layout
         self.__persistence_provider = persistence_provider
 
@@ -172,7 +172,9 @@ class Game:
         return {
             "game_id": self.id,
             "open_spots": self.__open_spots,
-            "game_status": self.game_status.value
+            "game_status": self.game_status.value,
+            "moves_next": self.moves_next,
+            "players": self.players
         }
     # -----------------------------------------------------------------------------------
     # LOAD METHOD
